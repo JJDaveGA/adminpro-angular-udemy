5a0c3b9e4e2177c1f586a921ccc3576c857dcc3f
David G.A <jjdave.git.22@gmail.com>
2018-08-11 13:51:29 -0500

    App module and routes fix
    App module and routes fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
+import { APP_ROUTES } from './app.routes';
+
 // Modules
 import { PagesModule } from './pages/pages.module';
 
@@ -8,9 +11,6 @@ import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
-// Routes
-import { APP_ROUTES } from './app.routes';
 
 @NgModule({
     declarations: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
-import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphic1Component } from './pages/graphic1/graphic1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
